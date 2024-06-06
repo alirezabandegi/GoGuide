8a55df5c68d087ee58a4d4172c32bc9c51a54d34
Alireza Bandegi <60815121+alirezabandegi@users.noreply.github.com>
2024-06-07 00:57:24 +0300

    homePage footer created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header.jsx";
 import HomePageMain from "./components/HomePageMain.jsx";
+import FooterCard from "./components/FooterCard.jsx";
 
 function App() {
   
@@ -7,6 +8,7 @@ function App() {
     <>
       <Header/>
       <HomePageMain/>
+      <FooterCard/>
     </>
   )
 }
